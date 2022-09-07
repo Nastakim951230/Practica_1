30ba82f0ee09ebe92fd644caaa55e76fcbfdd768
nasty <nastytrifono03@gmail.com>
2022-09-08 00:56:33 +0300

    Test commit

---
+++ app/src/main/java/com/example/practica_1/ConnectionHelper.java
@@ -0,0 +1,40 @@
+package com.example.practica_1;
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionHelper {
+
+    String userName, userPassword, ip, port, dataBase;
+
+    public Connection connection()
+    {
+        ip="ngknn.ru";
+        dataBase="Trifonova_41P";
+        userPassword="12357";
+        userName="31П";
+        port="1433";
+
+        StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Connection connection=null;
+        String ConnectionURL=null;
+
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";" + "databasename=" + dataBase + ";user=" + userName + ";password=" + userPassword + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
+
+        }
+
+        catch (Exception ex)
+        {
+            Log.e("Error", ex.getMessage());
+        }
+        return connection;
+    }
+}
