03d5b7c832ce462c99de514b419ff81dc4730970
nasty <nastytrifono03@gmail.com>
2022-09-20 22:33:15 +0300

    Updata

--- app/src/main/java/com/example/practica_1/UPDATE.java
+++ app/src/main/java/com/example/practica_1/UPDATE.java
@@ -11,27 +11,27 @@ import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class MainActivity extends AppCompatActivity {
+public class UPDATE extends AppCompatActivity {
 
-Connection connection;
-String ConnectionResult="";
-
-
+    Connection connection;
+    String ConnectionResult="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_update);
+        FromSql();
     }
 
-    public  void GetTextFromSql(View v)
+    public  void FromSql()
     {
 
-        TableLayout layout = findViewById(R.id.dbLayoutRow);
+        TableLayout layout = findViewById(R.id.dbUpdate);
         layout.removeAllViews();
 
 
@@ -48,8 +48,8 @@ String ConnectionResult="";
 
                 while (resultSet.next())
                 {
-                    TableRow dbLayoutRow = new TableRow(this);
-                    dbLayoutRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    TableRow dbUpdate = new TableRow(this);
+                    dbUpdate.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                             LinearLayout.LayoutParams.WRAP_CONTENT));
                     TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
                             TableRow.LayoutParams.WRAP_CONTENT);
@@ -61,7 +61,7 @@ String ConnectionResult="";
                     ID.setText(resultSet.getString(1));
                     ID.setTextSize(12);
                     ID.setGravity(Gravity.CENTER);
-                    dbLayoutRow.addView(ID);
+                    dbUpdate.addView(ID);
 
                     TextView Name = new TextView(this);
                     params.weight = 1.0f;
@@ -69,7 +69,7 @@ String ConnectionResult="";
                     Name.setText(resultSet.getString(2));
                     Name.setTextSize(12);
                     Name.setGravity(Gravity.CENTER);
-                    dbLayoutRow.addView(Name);
+                    dbUpdate.addView(Name);
 
                     TextView Surname = new TextView(this);
                     params.weight = 1.0f;
@@ -77,7 +77,7 @@ String ConnectionResult="";
                     Surname.setText(resultSet.getString(3));
                     Surname.setTextSize(12);
                     Name.setGravity(Gravity.CENTER);
-                    dbLayoutRow.addView(Surname);
+                    dbUpdate.addView(Surname);
 
 
                     TextView Floor = new TextView(this);
@@ -86,7 +86,9 @@ String ConnectionResult="";
                     Floor.setText(resultSet.getString(4));
                     Floor.setTextSize(12);
                     Name.setGravity(Gravity.CENTER);
-                    dbLayoutRow.addView(Floor);
+                    dbUpdate.addView(Floor);
+
+
 
                     TextView JobTitle = new TextView(this);
                     params.weight = 1.0f;
@@ -94,9 +96,9 @@ String ConnectionResult="";
                     JobTitle.setText(resultSet.getString(5));
                     JobTitle.setTextSize(12);
                     Name.setGravity(Gravity.CENTER);
-                    dbLayoutRow.addView(JobTitle);
+                    dbUpdate.addView(JobTitle);
 
-                    layout.addView(dbLayoutRow);
+                    layout.addView(dbUpdate);
 
                 }
             }
@@ -111,28 +113,52 @@ String ConnectionResult="";
         {
             Log.e("Ошибка", ex.getMessage());
         }
+    }
 
+    public void Update (View v)
+    {
+        TextView ID = findViewById(R.id.UPID);
+        TextView Name = findViewById(R.id.UPName);
+        TextView Surname = findViewById(R.id.UPSurname);
+        TextView Floor = findViewById(R.id.UPFloor);
+        TextView JobTitle = findViewById(R.id.UPJobTitle);
+
+        if (ID.getText().length()==0||Name.getText().length()==0|| Surname.getText().length()==0 || Floor.getText().length()==0 ||  JobTitle.getText().length()==0 )
+        {
+            Toast.makeText(this,"Не заполнены обязательные поля", Toast.LENGTH_LONG).show();
+            return;
+        }
+        try {
+            ConnectionHelper connectionHelper = new ConnectionHelper();
+            connection = connectionHelper.connectionClass();
+            if (connection != null) {
+                String query = "UPDATE Sotrudnic Set Name = '" + Name.getText() + "', Surname = '" + Surname.getText() + "', Floor ='" + Floor.getText() + "', Job_title ='" + JobTitle.getText() + "' WHERE ID= "+ID.getText()+"";
+                Statement statement = connection.createStatement();
+                ResultSet result = statement.executeQuery(query);
+                Toast.makeText(this,"Успешно изменено", Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Toast.makeText(this,"Произошла ошибка", Toast.LENGTH_LONG).show();
+        }
+        FromSql();
+        ID.setText("");
+        Name.setText("");
+        Surname.setText("");
+        Floor.setText("");
+        JobTitle.setText("");
 
     }
 
-    public void onClickAdd(View v) {
+    public void onClickPereho(View v) {
         switch (v.getId()) {
-            case R.id.btAdd:
-                Intent intent=new Intent(this,Adding.class);
+            case R.id.BTNazad:
+                Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 break;
         }
-
     }
-    public void onClickUpdate(View v) {
-        switch (v.getId()) {
-            case R.id.btUpdate2:
-                Intent intent=new Intent(this,UPDATE.class);
-                startActivity(intent);
-                break;
-        }
-
-    }
-
-
 }
